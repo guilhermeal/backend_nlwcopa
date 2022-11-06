4d962490c9e0619ab1bcb0b16a48537f8aa10f06
Guilherme Almeida <guiallan.al@gmail.com>
2022-11-06 00:46:04 -0300

    feat: create poll with logged users
    set first logged access at new owner poll if dont have one

--- src/routes/poll.ts
+++ src/routes/poll.ts
@@ -2,12 +2,13 @@ import { z } from "zod";
 import { FastifyInstance } from "fastify"
 import ShortUniqueId from "short-unique-id"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate";
 
 export async function pollRoutes(fastify: FastifyInstance) {
   fastify.get('/polls/count', async () => {
     const count = await prisma.poll.count()
     return { count }
-  })
+  });
 
   fastify.post('/polls', async (request, reply) => {
         
@@ -19,14 +20,87 @@ export async function pollRoutes(fastify: FastifyInstance) {
     const generate = new ShortUniqueId({ length: 6 })
     const code = String(generate()).toUpperCase();
 
+    try {
+      await request.jwtVerify()
 
-    await prisma.poll.create({
+      await prisma.poll.create({
         data: {
             title,
-            code
+            code,
+            ownerId: request.user.sub,
+
+            participants: {
+              create: {
+                userId: request.user.sub,
+              }
+            }
         }
-    })
+      });
+
+    } catch {
+      await prisma.poll.create({
+          data: {
+              title,
+              code
+          }
+      });
+    }
 
     return reply.status(201).send({ code })
-})
+  });
+
+  fastify.post('/polls/:id/join', {
+    onRequest: [authenticate]
+  }, async (request, reply) => {
+    const joinPollBody = z.object({
+      code: z.string(),
+    });
+
+    const { code } = joinPollBody.parse(request.body);
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        code,
+      }, 
+      include: {
+        participants: {
+          where: {
+            userId: request.user.sub
+          }
+        }
+      }
+    });
+
+    if(!poll) {
+      return reply.status(400).send({
+        message: "Poll not found!"
+      });
+    }
+
+    if(poll.participants.length > 0) {
+      return reply.status(400).send({
+        message: "You already joined this poll."
+      });
+    }
+
+    if(!poll.ownerId) {
+      await prisma.poll.update({
+        where: {
+          id: poll.id,
+        }, 
+        data: {
+          ownerId: request.user.sub
+        }
+      });
+    }
+
+    await prisma.participant.create({
+      data: {
+        pollId: poll.id,
+        userId: request.user.sub,
+      }
+    });
+
+    return reply.status(201).send();
+  }); 
 }
